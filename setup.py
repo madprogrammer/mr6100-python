f42c143477bf2f842f7b73cfef074d4915aed873
Sergey Anufrienko <serg@anufrienko.net>
2020-04-21 13:36:37 +0300

    prepare for pypi

--- setup.py
+++ setup.py
@@ -8,5 +8,14 @@ setup(
     license='MIT',
     author='Sergey Anufrienko',
     author_email='serg@anufrienko.net',
-    description='Library to interface with Marktrace MR6100 series UHF RFID reader'
+    description='Library to interface with Marktrace MR6100 series UHF RFID reader',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ]
 )
